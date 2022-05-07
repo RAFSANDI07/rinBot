827f4f7fd248cf366c861d3c0baffc6d06d7d4f1
Rafly <89924947+RAFSANDI07@users.noreply.github.com>
2022-05-07 15:22:21 +0700

    syntax

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from http import client
 import discord
 from discord.ext import commands
+import os
+import music
 
-client = commands.Bot(command_prefix='/')
 
-@client.event
-async def on_ready():
-    print('Rin has Active on your server.')
+cogs = [music]
 
-client.run('OTcxMjY2NDgyMzc1ODg0ODEw.GjDLGt.wKt86c1yEVC9M-JuDCU1M75zjezsywhtLcxCo0')
+client = commands.Bot(command_prefix='>', intents = discord.Intents.all())
+
+for i in range (len(cogs)):
+    cogs[i].setup()
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+# Testing
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+#
+
+client = MyClient()
+client.run(os.getenv("TOKEN"))
